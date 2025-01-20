38e7d909dcd0bd4dbd59791435a3e40dd5953697
Dusan Dimic <dusan.dimic@vegait.rs>
2025-01-20 15:36:28 +0100

    Adds a producer/consumer apps for managing driver location

---
+++ cab-book-driver/src/main/java/com/dusandimic/cab_book_driver/service/CabLocationService.java
@@ -0,0 +1,22 @@
+package com.dusandimic.cab_book_driver.service;
+
+import com.dusandimic.cab_book_driver.constant.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    public CabLocationService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public boolean updateLocation(String location) {
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+        return true;
+    }
+}
